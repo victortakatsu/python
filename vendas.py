b49255831bbeb0b2e9d38477becbde1f7313a9b3
Victor <110040376+victortakatsu@users.noreply.github.com>
2022-08-18 14:50:21 -0300

    v1.1.1
    Correções no código, transformando a variável vendas em float após a verificação do campo ser diferente de "vazio".

--- vendas.py
+++ vendas.py
@@ -1,20 +1,25 @@
 import time
-
+print('-------------------------------')
+print('-------------------------------')
 meta = 15000
 nome = input('Olá! Por favor, digite seu nome: ')
-vendas = float(input('Pronto, {}. Agora insira o valor total de suas vendas: '.format(nome)))
+print('-------------------------------')
+vendas = input('Pronto, {}. Agora insira o valor total de suas vendas: '.format(nome))
 
-print('Ok, verificando as informações. Aguarde, por favor.')
-time.sleep(3)
-
-if vendas:
+if vendas != '':
+    vendas = float(vendas)
+    time.sleep(1)
+    print('---------------------------------------')
+    print('Ok, verificando as informações. Aguarde, por favor.')
+    print('---------------------------------------')
+    time.sleep(3)
     if vendas >= (meta * 2):
         bonus = 0.07 * vendas
         print('Uau! {}, você vendeu mais que o dobro da meta deste mês! Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
-    elif vendas > meta:
+    elif vendas >= meta:
         bonus = 0.03 * vendas
-        print('Parabéns {}! Suas vendas ultrapassaram a meta desse mês. Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
+        print('Parabéns {}! Suas vendas alcançaram a meta desse mês. Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
     else:
         print('Poxa, {}. Dessa vez não vai rolar aquele bônus. Continue se empenhando para garanti-lo no próximo mês!'.format(nome))
 else:
-    print('Erro! Preencha o campo de vendas corretamente.')
+    print('Preencha o campo de vendas corretamente.')
