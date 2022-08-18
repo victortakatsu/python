5d9563574e87e88c3dd947bfa6e71d5e963b3835
Victor <110040376+victortakatsu@users.noreply.github.com>
2022-08-18 14:35:59 -0300

    v1.1
    Adicionado verificação do campo de vendas, caso não contenha nenhuma informação.

--- vendas.py
+++ vendas.py
@@ -1,16 +1,20 @@
+import time
+
 meta = 15000
 nome = input('Olá! Por favor, digite seu nome: ')
 vendas = float(input('Pronto, {}. Agora insira o valor total de suas vendas: '.format(nome)))
-import time
 
 print('Ok, verificando as informações. Aguarde, por favor.')
 time.sleep(3)
 
-if vendas >= (meta * 2):
-    bonus = 0.07 * vendas
-    print('Uau! {}, você vendeu mais que o dobro da meta deste mês! Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
-elif vendas > meta:
-    bonus = 0.03 * vendas
-    print('Parabéns {}! Suas vendas ultrapassaram a meta desse mês. Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
+if vendas:
+    if vendas >= (meta * 2):
+        bonus = 0.07 * vendas
+        print('Uau! {}, você vendeu mais que o dobro da meta deste mês! Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
+    elif vendas > meta:
+        bonus = 0.03 * vendas
+        print('Parabéns {}! Suas vendas ultrapassaram a meta desse mês. Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
+    else:
+        print('Poxa, {}. Dessa vez não vai rolar aquele bônus. Continue se empenhando para garanti-lo no próximo mês!'.format(nome))
 else:
-    print('Poxa, {}. Dessa vez não vai rolar aquele bônus. Continue se empenhando para garanti-lo no próximo mês!'.format(nome))
+    print('Erro! Preencha o campo de vendas corretamente.')
