a2822873e8694a0d52fba59f78038412bf713fab
Victor <110040376+victortakatsu@users.noreply.github.com>
2022-08-18 17:07:56 -0300

    v1.1.2
    Adicionado verificadores de campos vazios. Caso não seja inserido um nome, o programa não dá continuidade;
    Caso o valor digitado no campo "vendas" não seja numérico ou fique vazio, ele informa ao usuário para digitar apenas números;

--- vendas.py
+++ vendas.py
@@ -4,22 +4,26 @@ print('-------------------------------')
 meta = 15000
 nome = input('Olá! Por favor, digite seu nome: ')
 print('-------------------------------')
-vendas = input('Pronto, {}. Agora insira o valor total de suas vendas: '.format(nome))
+if nome != '':
+    nome = nome.capitalize()
+    vendas = input('Pronto, {}. Agora insira o valor total de suas vendas: '.format(nome))
 
-if vendas != '':
-    vendas = float(vendas)
-    time.sleep(1)
-    print('---------------------------------------')
-    print('Ok, verificando as informações. Aguarde, por favor.')
-    print('---------------------------------------')
-    time.sleep(3)
-    if vendas >= (meta * 2):
-        bonus = 0.07 * vendas
-        print('Uau! {}, você vendeu mais que o dobro da meta deste mês! Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
-    elif vendas >= meta:
-        bonus = 0.03 * vendas
-        print('Parabéns {}! Suas vendas alcançaram a meta desse mês. Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
+    if vendas.isnumeric():
+        vendas = float(vendas)
+        time.sleep(1)
+        print('---------------------------------------')
+        print('Ok, verificando as informações. Aguarde, por favor.')
+        print('---------------------------------------')
+        time.sleep(3)
+        if vendas >= (meta * 2):
+            bonus = 0.07 * vendas
+            print('Uau! {}, você vendeu mais que o dobro da meta deste mês! Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
+        elif vendas >= meta:
+            bonus = 0.03 * vendas
+            print('Parabéns {}! Suas vendas alcançaram a meta desse mês. Você receberá um bônus de R$ {:.2f}.'.format(nome, bonus))
+        else:
+            print('Poxa, {}. Dessa vez não vai rolar aquele bônus. Continue se empenhando para garanti-lo no próximo mês!'.format(nome))
     else:
-        print('Poxa, {}. Dessa vez não vai rolar aquele bônus. Continue se empenhando para garanti-lo no próximo mês!'.format(nome))
+        print('Por favor, preencha o campo de vendas corretamente: utilize apenas números.')
 else:
-    print('Preencha o campo de vendas corretamente.')
+    print('Desculpe, não consigo te identificar sem um nome. Até logo.')
